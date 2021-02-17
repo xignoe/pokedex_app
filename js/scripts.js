deb4d35305f6a66b11c2eddf6c0b76fda3496838
xignoe <51763652+xignoe@users.noreply.github.com>
2021-02-17 17:05:57 -0500

    Created pokemon array
    Removed old code and replaced with array containing data on six pokemon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
-alert('Hello world');
-
-let favoriteFood = "grilled cheese";
-
-document.write(favoriteFood);
-
+let pokemonList = [
+    { name: 'Cloyster', height: 1.5, types: ['ice','water'] }, 
+    { name: 'Charizard', height: 1.7, types: ['fire', 'flying'] },
+    { name: 'Gengar', height: 1.5, types: ['ghost', 'poison'] },
+    { name: 'Electrode', height: 1.2, types: ['electric'] },
+    { name: 'Dragonite', height: 2.2, types: ['dragon', 'flying'] },
+    { name: 'Exeggutor', height: 2, types: ['psychic', 'grass'] }
+]
