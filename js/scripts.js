4bf5a691828f39435881650107b136499ca1a423
xignoe <51763652+xignoe@users.noreply.github.com>
2021-02-18 15:55:10 -0500

    Updated with for loop
    Added a for loop with a conditional statement that writes the name and height of the Pokemon, adding the message "Wow that is big!" if the pokemon is above a certain height.

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,17 @@ let pokemonList = [
     { name: 'Electrode', height: 1.2, types: ['electric'] },
     { name: 'Dragonite', height: 2.2, types: ['dragon', 'flying'] },
     { name: 'Exeggutor', height: 2, types: ['psychic', 'grass'] }
-]
+];
+
+console.log("test");
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height >= 2.2) {
+        console.log("Wow that's big!");
+        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow that is big!' + '</p>');
+    } else {
+        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+    }
+
+
+}
