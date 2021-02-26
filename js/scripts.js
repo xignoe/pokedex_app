50d2fe417b291447c58ea8b095d13aa299d45588
xignoe <51763652+xignoe@users.noreply.github.com>
2021-02-25 20:25:24 -0500

    Updated pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,41 @@
-let pokemonList = [
-    { name: 'Cloyster', height: 1.5, types: ['ice','water'] }, 
-    { name: 'Charizard', height: 1.7, types: ['fire', 'flying'] },
-    { name: 'Gengar', height: 1.5, types: ['ghost', 'poison'] },
-    { name: 'Electrode', height: 1.2, types: ['electric'] },
-    { name: 'Dragonite', height: 2.2, types: ['dragon', 'flying'] },
-    { name: 'Exeggutor', height: 2, types: ['psychic', 'grass'] }
-];
+// Commented out the IIFE wrapping because the index.html does not display anything otherwise, please advise.
+//(function () { 
+    let pokemonList = [
+        { name: 'Cloyster', height: 1.5, types: ['ice','water'] }, 
+        { name: 'Charizard', height: 1.7, types: ['fire', 'flying'] },
+        { name: 'Gengar', height: 1.5, types: ['ghost', 'poison'] },
+        { name: 'Electrode', height: 1.2, types: ['electric'] },
+        { name: 'Dragonite', height: 2.2, types: ['dragon', 'flying'] },
+        { name: 'Exeggutor', height: 2, types: ['psychic', 'grass'] }
+    ];
+//})();
 
-console.log("test");
+let pokemonRepository = (function () {
+    return {
+        getAll: function() {
+            return pokemonList;
+        },
 
-pokemonList.forEach(function(pokemon){
-    if (pokemon.height >= 2.2) {
-        console.log("Wow that's big!");
-        document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') - Wow that is big!' + '</p>');
-    } else {
-        document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')');
-    }
+        add: function(pokemon) {
+            pokemonList.push(pokemon);
+        }
+    };
+})();
 
-})
+(function () {
+
+    pokemonList.forEach(function(pokemon){
+        let pokeName = pokemon.name;
+        let pokeHeight = pokemon.height;
+
+        if (pokeHeight >= 2.2) {
+            console.log("Wow that's big!");
+            document.write('<p>' + pokeName + ' (height: ' + pokeHeight + ') - Wow that is big!' + '</p>');
+        } else {
+            document.write('<p>' + pokeName + ' (height: ' + pokeHeight + ')');
+        }
+    
+    })
+
+})();
+
